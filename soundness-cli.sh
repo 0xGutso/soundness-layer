c8e0e0cd54ca62e4c22b2d5b7ddbdc6d7546af14
gutso <65046349+gutsoo@users.noreply.github.com>
2025-03-21 20:39:26 +0300

    Update soundness-cli.sh

--- soundness-cli.sh
+++ soundness-cli.sh
@@ -3,22 +3,21 @@
 curl -s https://raw.githubusercontent.com/0xGutso/logo/refs/heads/main/logo.sh | bash
 sleep 3
 
-echo "Kurulum başlatılıyor"
-sleep 1
-
+# Docker kontrolü ve kurulumu
 if ! command -v docker &> /dev/null 
-    then 
-        echo "Docker bulunamadı, Docker yükleniyor..."
-        sudo apt update && sudo apt upgrade -y
-    else 
+then 
+    echo "Docker bulunamadı, Docker yükleniyor..."
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install -y docker.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+else 
     echo "Docker zaten cihazda kurulu, sonraki aşamaya geçiliyor."
 fi
 
-echo "Rust kuruluyor"
+# Rust kontrolü ve kurulumu
+echo "Rust kuruluyor..."
 sleep 2
-echo 
-
-echo 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 rustc --version
@@ -26,23 +25,25 @@ cargo --version
 echo 'source $HOME/.cargo/env' >> ~/.bashrc
 echo "Rust kurulumu tamamlandı, sonraki aşamaya geçiliyor."
 sleep 3
-echo 
 
+# Soundness kurulumu
 echo "Soundness kurulumu yapılıyor..."
 sleep 2
 curl -sSL https://raw.githubusercontent.com/soundnesslabs/soundness-layer/main/soundnessup/install | bash
 source ~/.bashrc
 soundnessup install
+sleep 2
 soundnessup update
-echo 
 
+# Key oluşturma
 echo "Kurulum yapıldı, lütfen bir sonraki aşamada verilecek olan mnemonic kelimelerini kaydedin! Ardından şifrenizi iki defa girin."
 sleep 6
 
+echo "Key oluşturuluyor.."
+sleep 3
 soundness-cli generate-key --name my-key
 
-echo "Yukarıda verilen public key için lütfen Readme.MD dosyasına göz atmayı unutmayın."
-sleep 2
-echo
-
+sleep 3
+chmod +x install_soundness.sh
+./install_soundness.sh
 
