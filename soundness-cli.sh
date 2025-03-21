dbedd5ab84e1e2f1215fe59bb7cfda703b348867
gutso <65046349+gutsoo@users.noreply.github.com>
2025-03-21 20:27:31 +0300

    Update soundness-cli.sh

--- soundness-cli.sh
+++ soundness-cli.sh
@@ -16,15 +16,17 @@ fi
 
 echo "Rust kuruluyor"
 sleep 2
+echo 
 
+echo 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 rustc --version
 cargo --version
 echo 'source $HOME/.cargo/env' >> ~/.bashrc
-
 echo "Rust kurulumu tamamlandı, sonraki aşamaya geçiliyor."
 sleep 3
+echo 
 
 echo "Soundness kurulumu yapılıyor..."
 sleep 2
@@ -32,7 +34,7 @@ curl -sSL https://raw.githubusercontent.com/soundnesslabs/soundness-layer/main/s
 source ~/.bashrc
 soundnessup install
 soundnessup update
-
+echo 
 
 echo "Kurulum yapıldı, lütfen bir sonraki aşamada verilecek olan mnemonic kelimelerini kaydedin! Ardından şifrenizi iki defa girin."
 sleep 6
@@ -41,7 +43,6 @@ soundness-cli generate-key --name my-key
 
 echo "Yukarıda verilen public key için lütfen Readme.MD dosyasına göz atmayı unutmayın."
 sleep 2
-
-echo "Tüm işlemler bu kadardı, bol şans."
+echo
 
 
